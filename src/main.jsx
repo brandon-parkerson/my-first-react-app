9e3b5fed450d0789bfc9d8a27afdea19a66fada4
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-01-12 19:28:03 +0200

    hello world

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <h1>Hello, World!</h1>
+  
 )
